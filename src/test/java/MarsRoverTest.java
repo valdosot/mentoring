08ddc751ae483410b3bc95432d2d31658758e825
Waldemar Sotillo <valdosot@gmail.com>
2023-02-13 16:20:06 +0100

    posY_Plus_One_Test

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -27,6 +27,7 @@ public class MarsRoverTest {
         Assertions.assertEquals("I", surface[initial_position_x - 1][initial_position_y - 1].trim());
         Assertions.assertEquals("D", surface[wished_target_position_x - 1][wished_target_position_y - 1].trim());
     }
+
     @Test
     void posX_Minus_One_Test() {
         int movement_route_x = -1;
@@ -34,6 +35,20 @@ public class MarsRoverTest {
 
         int wished_target_position_x = 4;
         int wished_target_position_y = 1;
+
+        String[][] surface = marsRover.marsSurface(initial_position_x, initial_position_y, movement_route_x, movement_route_y);
+
+        Assertions.assertEquals("I", surface[initial_position_x - 1][initial_position_y - 1].trim());
+        Assertions.assertEquals("D", surface[wished_target_position_x - 1][wished_target_position_y - 1].trim());
+    }
+
+    @Test
+    void posY_Plus_One_Test() {
+        int movement_route_x = 0;
+        int movement_route_y = 1;
+
+        int wished_target_position_x = 1;
+        int wished_target_position_y = 2;
 
         String[][] surface = marsRover.marsSurface(initial_position_x, initial_position_y, movement_route_x, movement_route_y);
 
