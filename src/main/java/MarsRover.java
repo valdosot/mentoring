d9a33b11bdb6938d81cc899cdff6ec280e083485
Waldemar Sotillo <valdosot@gmail.com>
2023-02-13 16:18:11 +0100

    Initial Commit posX_Plus_One_Test

---
+++ src/main/java/MarsRover.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class MarsRover {
+    public String[][] marsSurface(int posIni_x, int posIni_y, int posDes_x, int posDes_y) {
+        String[][] surface = new String[4][4];
+
+        return surface;
+    }
+}
---
+++ src/test/java/MarsRoverTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MarsRoverTest {
+    private MarsRover marsRover;
+    private int initial_position_x;
+    private int initial_position_y;
+
+    @BeforeEach
+    void setUp() {
+        marsRover = new MarsRover();
+        initial_position_x = 1;
+        initial_position_y = 1;
+    }
+
+    @Test
+    void posX_Plus_One_Test() {
+        int movement_route_x = 1;
+        int movement_route_y = 0;
+
+        int wished_target_position_x = 2;
+        int wished_target_position_y = 1;
+
+        String[][] surface = marsRover.marsSurface(initial_position_x, initial_position_y, movement_route_x, movement_route_y);
+
+        Assertions.assertEquals("I", surface[initial_position_x - 1][initial_position_y - 1].trim());
+        Assertions.assertEquals("D", surface[wished_target_position_x - 1][wished_target_position_y - 1].trim());
+    }
+
+}
